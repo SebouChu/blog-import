4a03cb0a9b5aa280e05cc3604f49b3be197582ea
Sébastien Gaya <sebastien.gaya@gmail.com>
2026-02-23 09:44:20 +0100

    add export_aliases

---
+++ bin/export_aliases.rb
@@ -0,0 +1,60 @@
+require 'rubygems'
+require 'bundler/setup'
+
+Bundler.require(:default)
+Dotenv.load
+
+OsunyApi.configure do |config|
+  config.api_key['X-Osuny-Token'] = ENV['OSUNY_API_TOKEN']
+  config.host = 'la27eregion.osuny.org'
+  config.base_path = '/api/osuny/v1'
+  # config.debugging = true
+end
+
+API = OsunyApi::CommunicationWebsitePostApi.new
+SOURCE_DIRECTORY = './converted_posts/'
+
+def export_id(id)
+  puts "Export id #{id}"
+  export_path "#{SOURCE_DIRECTORY}#{id}.json"
+end
+
+def export_path(path)
+  puts "Export path #{path}"
+  file = File.read path
+  object = JSON.parse file
+  post = {
+    migration_identifier: object['migration_identifier'],
+    localizations: object['localizations'].map { |iso_code, l10n|
+      [
+        iso_code,
+        {
+          migration_identifier: l10n['migration_identifier'],
+          aliases: l10n['aliases'].map { |alias_path| { path: alias_path } }
+        }
+      ]
+    }.to_h
+  }
+  data = {
+    body: {
+      posts: [
+        post
+      ]
+    }
+  }
+  begin
+    result = API.communication_websites_website_id_posts_upsert_post(ENV['OSUNY_WEBSITE_ID'], data)
+  rescue OsunyApi::ApiError => e
+    puts "Exception when calling CommunicationWebsitePostApi->communication_websites_website_id_posts_upsert_post: #{e}"
+  end
+end
+
+def export_directory
+  puts "Export directory"
+  Dir["#{SOURCE_DIRECTORY}*.json"].each do |path|
+    export_path path
+  end
+end
+
+ARGV.empty? ? export_directory
+            : export_id(ARGV.first)
